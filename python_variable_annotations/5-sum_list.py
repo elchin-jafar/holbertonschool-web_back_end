8fd2d706084078303f5e8cfb692af8500a802c8b
Elchin <elcinceferli@gmail.com>
2024-04-29 16:43:28 +0400

    import list type

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """takes input list return sum list"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """return sum of list elements"""
     sum: float = 0
     for input in input_list:
