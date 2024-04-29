0ad26772ebb84c151c3d310776f98f94316539f4
Elchin <elcinceferli@gmail.com>
2024-04-29 16:48:09 +0400

    remove type keyword

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,7 +2,7 @@
 """sum mixed list"""
 from typing import List, Union
 
-type UnionList = List[Union[int, float]]
+UnionList = List[Union[int, float]]
 
 
 def sum_mixed_list(mxd_list: UnionList) -> float:
