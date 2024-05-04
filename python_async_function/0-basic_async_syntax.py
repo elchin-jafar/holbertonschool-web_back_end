4bb7c4284b0dcc1d833100b09262249a6af0ace3
Elchin <elcinceferli@gmail.com>
2024-05-05 00:37:29 +0400

    change type

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
         generate random float num in range 0-10
         wait for that generated number as time then return it
