80a15a110eba698b0502787d5efdf4dbd10e41dc
Elchin <elcinceferli@gmail.com>
2024-05-05 01:04:46 +0400

    add docs

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,7 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """multiple coroutines"""
     result: List = []
     for _ in range(n):
         result.append(await wait_random(max_delay))
