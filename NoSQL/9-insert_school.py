30e8d29308dafa4dfc9dbe81d4881b2d69c7bd49
Elchin <elcinceferli@gmail.com>
2024-05-09 18:24:44 +0400

    return exact id

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """insert a document in python"""
 
-import pymongo
-
 
 def insert_school(mongo_collection, **kwargs):
     """insert given data to collection based on kwargs"""
-    return db.mongo_collection.insert(kwargs)
+    return db.mongo_collection.insert(kwargs).insertedIds['0']
