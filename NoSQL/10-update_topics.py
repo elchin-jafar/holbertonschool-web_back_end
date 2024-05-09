72003c4a9d14f8a8fd1c1031a8f2e91a64a3ff27
Elchin <elcinceferli@gmail.com>
2024-05-09 18:47:28 +0400

    multi

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,4 +4,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """change all topics of a school document based on the name"""
-    mongo_collection.update({"name": name}, {$set: {"topics": topics}})
+    mongo_collection.update({"name": name}, {$set: {"topics": topics}}, {multi: true})
