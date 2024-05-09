3b3239cb38bde714f20b435664524a04055c2045
Elchin <elcinceferli@gmail.com>
2024-05-09 18:12:15 +0400

    insert

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""insert a document in python"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert given data to collection based on kwargs"""
+    db.mongo_collection.insert(kwargs)
