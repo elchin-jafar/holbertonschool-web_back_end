1f9c1ed20deffd91d59a4d106a9506d4b512da5d
Elchin <elcinceferli@gmail.com>
2024-05-11 01:38:58 +0400

    pagination index_range

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""pagination"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    return ((page - 1) * page_size, page * page_size)
