74194c5a59ace2aad4271aa47e1adc6fee81c55f
Elchin <elcinceferli@gmail.com>
2024-05-11 01:41:35 +0400

    add docs

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,4 +4,5 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """simple helper func return start and ending indexes"""
     return ((page - 1) * page_size, page * page_size)
