821d04ca514d25b21fadaccc7a3885e093c05048
Elchin <elcinceferli@gmail.com>
2024-05-10 16:17:27 +0400

    add status check

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -20,3 +20,7 @@ methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 for method in methods:
     count = collection.count_documents({"method": method})
     print("method {}: {}".format(method, count))
+
+countGet = collection.count_documents({"method": "GET", "path": "/status"})
+
+print("{} status check".format(countGet))
